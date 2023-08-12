3f75807415614247ca7478443ef0c1470c0d469d
agfreckles <oluueli@gmail.com>
2023-08-12 20:58:09 +0000

    Change revert

--- suacodebackend/settings.py
+++ suacodebackend/settings.py
@@ -26,13 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 print('this', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = env('DJANGO_DEBUG') == 'False'
 print(DEBUG)
 
 ALLOWED_HOSTS = ['*']
