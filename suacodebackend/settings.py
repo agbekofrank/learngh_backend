15decc65a78ee03027f08b3118d08f76ba218154
agfreckles <oluueli@gmail.com>
2023-08-12 21:37:06 +0000

    Prefix env variable names with DJANGO

--- suacodebackend/settings.py
+++ suacodebackend/settings.py
@@ -29,9 +29,9 @@ load_dotenv(os.path.join(BASE_DIR,".env"))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
-SECRET_KEY = os.getenv(environ_name="DJANGO_SECRET_KEY")
+# SECRET_KEY = os.getenv(environ_name="DJANGO_SECRET_KEY")
 
 
 print('this', SECRET_KEY)
